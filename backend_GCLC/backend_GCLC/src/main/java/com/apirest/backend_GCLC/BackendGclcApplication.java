ce1beb7d7bb2b36d9bb56a08c900fd88e031bec2
xcamilaavila <camila.paz@uao.edu.co>
2025-10-21 09:12:34 -0500

    Add files via upload

---
+++ backend_GCLC/backend_GCLC/src/main/java/com/apirest/backend_GCLC/BackendGclcApplication.java
@@ -0,0 +1,13 @@
+package com.apirest.backend_GCLC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendGclcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendGclcApplication.class, args);
+	}
+
+}
---
+++ backend_GCLC/backend_GCLC/src/test/java/com/apirest/backend_GCLC/BackendGclcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apirest.backend_GCLC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendGclcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
